66936d13ca62a3d5cf823389d6ec251775c6e4ce
Lu <elgranchipiron@outlook.es>
2025-12-15 17:23:29 +0100

    modificado catalogación del dropdown menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { CarouselComponent } from '../carousel/carousel.component';
 })
 export class HomeComponent implements OnInit {
   imageUrls: string[] = [
-    'https://www.humaneworld.org/sites/default/files/styles/sa_social_media_facebook/public/2022-07/kitten-playing-575035.jpg?h=b1b36da8&itok=0HJldiWn',
+    'https://content3.cdnprado.net/imagenes/proyectos/personalizacion/7317a29a-d846-4c54-9034-6a114c3658fe/cms/meninas-antes.jpg',
     'https://www.floppycats.com/wp-content/uploads/2023/08/tricolor-kitten-is-playing-on-a-white-background-in-front-of-the-camera-1.jpg',
     'https://img.freepik.com/premium-photo/kitten-white-background_78621-487.jpg',
   ];
